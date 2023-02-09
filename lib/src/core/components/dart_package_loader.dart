58f8ba70e7ed75ca2b705a454aa5a2d004f60410
Gustavo Hillesheim <guhillesheim@gmail.com>
2023-02-09 07:15:30 -0300

    update files

--- lib/src/core/components/dart_package_loader.dart
+++ lib/src/core/components/dart_package_loader.dart
@@ -4,9 +4,10 @@ import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/file_system/physical_file_system.dart';
 import 'package:path/path.dart';
-import '../models/models.dart';
-import '../mappers/mappers.dart';
+
 import '../../utils.dart';
+import '../mappers/mappers.dart';
+import '../models/models.dart';
 
 class DartPackageLoader {
   static late DartPackageLoader instance = DartPackageLoader(
