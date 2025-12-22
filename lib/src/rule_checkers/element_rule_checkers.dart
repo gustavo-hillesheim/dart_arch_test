7a37dbdc99c5112b6f1a1a5a2183f31e4fb6328c
Gustavo Hillesheim <guhillesheim@gmail.com>
2025-12-22 15:11:07 -0300

    Created example RuleChecker

---
+++ lib/src/rule_checkers/element_rule_checkers.dart
@@ -0,0 +1,24 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'package:arch_test/arch_test.dart';
+
+HaveNameEndingWithRuleChecker<E> haveNameEndingWith<E extends Element>(
+    String suffix) {
+  return HaveNameEndingWithRuleChecker<E>(suffix);
+}
+
+class HaveNameEndingWithRuleChecker<E extends Element> extends RuleChecker<E> {
+  final String suffix;
+
+  HaveNameEndingWithRuleChecker(this.suffix);
+
+  @override
+  void check(Element element, RuleViolationCollector violationCollector) {
+    final name = element.name;
+    if (name == null || !name.endsWith(suffix)) {
+      violationCollector.error(
+        element,
+        '$E "$name" does not have name ending with "$suffix".',
+      );
+    }
+  }
+}
