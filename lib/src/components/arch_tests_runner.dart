2faedf3ff8010b4573f4c33eba6e4c8047b055f1
Gustavo Hillesheim <guhillesheim@gmail.com>
2025-12-22 17:07:07 -0300

    Created ArchTestsRunner

---
+++ lib/src/components/arch_tests_runner.dart
@@ -0,0 +1,41 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'package:arch_test/arch_test.dart';
+
+class ArchTestsRunner {
+  const ArchTestsRunner({
+    required this.declaredRules,
+    required this.packageLibraries,
+  });
+
+  final List<ArchRule> declaredRules;
+  final List<LibraryElement> packageLibraries;
+
+  void checkRules() {
+    final violationsCollector = RuleViolationCollector();
+    final allLibrariesAndTopLevelElements = packageLibraries
+        .expand((library) => [
+              library,
+              ...library.children,
+            ])
+        .toList();
+    for (final rule in declaredRules) {
+      _checkRule(
+        rule,
+        allLibrariesAndTopLevelElements,
+        violationsCollector,
+      );
+    }
+  }
+
+  void _checkRule(
+    ArchRule rule,
+    List<Element> packageElements,
+    RuleViolationCollector violationsCollector,
+  ) {
+    final candidateElements =
+        rule.elementMatcher.findMatchingElementsIn(packageElements);
+    for (final element in candidateElements) {
+      rule.checker.check(element, violationsCollector);
+    }
+  }
+}
