271a33e8372acce34de3d89e690f3e455b37305f
Gustavo Hillesheim <guhillesheim@gmail.com>
2025-12-26 13:16:34 -0300

    Changed ArchRule and ElementSelector to implement ElementPredicate

--- lib/src/core/models/arch_rule.dart
+++ lib/src/core/models/arch_rule.dart
@@ -1,9 +1,13 @@
 import 'package:analyzer/dart/element/element.dart';
+import 'package:arch_test/arch_test.dart';
 
-import '../components/components.dart';
-
-abstract class ArchRule<E extends Element> {
-  String describe();
-
+abstract class ArchRule<E extends Element> implements ElementPredicate<E> {
   void check(E element, ArchRuleViolationsCollector collector);
+
+  @override
+  bool satisfies(E element) {
+    final collector = ArchRuleViolationsCollector();
+    check(element, collector);
+    return collector.allViolations.isEmpty;
+  }
 }
