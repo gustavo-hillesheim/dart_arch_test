ee48a5e36b89718eec0e14c8570f997343964d2f
Gustavo Hillesheim <guhillesheim@gmail.com>
2025-12-29 17:46:37 -0300

    Named example selectors

--- example/test/arch_test/selectors.dart
+++ example/test/arch_test/selectors.dart
@@ -1,10 +1,34 @@
 import 'package:arch_test/arch_test.dart';
 
-final entities = classes.that(haveNameEndingWith('Entity'));
-final repositories = classes.that(haveNameEndingWith('Repository'));
-final repositoryImpls = classes.that(haveNameEndingWith('RepositoryImpl'));
-final usecases = classes.that(haveNameEndingWith('UseCase'));
-final usecasesImpls = classes.that(haveNameEndingWith('UseCaseImpl'));
-final datasources = classes.that(haveNameEndingWith('DataSource'));
-final datasourcesImpls = classes.that(haveNameEndingWith('DataSourceImpl'));
-final controllers = classes.that(haveNameEndingWith('Controller'));
+final entities = NamedElementSelector(
+  classes.that(haveNameEndingWith('Entity')),
+  'entities',
+);
+final repositories = NamedElementSelector(
+  classes.that(haveNameEndingWith('Repository')),
+  'repositories',
+);
+final repositoryImpls = NamedElementSelector(
+  classes.that(haveNameEndingWith('RepositoryImpl')),
+  'repository implementations',
+);
+final usecases = NamedElementSelector(
+  classes.that(haveNameEndingWith('UseCase')),
+  'usecases',
+);
+final usecasesImpls = NamedElementSelector(
+  classes.that(haveNameEndingWith('UseCaseImpl')),
+  'usecases implementations',
+);
+final datasources = NamedElementSelector(
+  classes.that(haveNameEndingWith('DataSource')),
+  'datasources',
+);
+final datasourcesImpls = NamedElementSelector(
+  classes.that(haveNameEndingWith('DataSourceImpl')),
+  'datasources implementations',
+);
+final controllers = NamedElementSelector(
+  classes.that(haveNameEndingWith('Controller')),
+  'controllers',
+);
