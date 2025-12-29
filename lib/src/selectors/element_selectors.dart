c564bdf5e11757a5cf642d5413cbf9016e858060
Gustavo Hillesheim <guhillesheim@gmail.com>
2025-12-29 17:39:25 -0300

    Created CombiningElementSelector

--- lib/src/selectors/element_selectors.dart
+++ lib/src/selectors/element_selectors.dart
@@ -8,6 +8,10 @@ extension ElementSelectorExtension<E extends Element, O extends Element>
   ElementSelector<E, O> that(ElementPredicate<O> filter) {
     return FilteringElementSelector<E, O>(this, filter);
   }
+
+  ElementSelector<E, O> and(ElementSelector<E, O> other) {
+    return CombiningElementSelector<E, O>(this, other);
+  }
 }
 
 class TypeElementSelector<E extends Element>
@@ -42,5 +46,26 @@ class FilteringElementSelector<E extends Element, O extends Element>
   @override
   List<O> select(List<E> elements) {
     return source.select(elements).where(filter.satisfies).toList();
+  }
+}
+
+class CombiningElementSelector<E extends Element, O extends Element>
+    extends ElementSelector<E, O> {
+  final ElementSelector<E, O> first;
+  final ElementSelector<E, O> second;
+
+  CombiningElementSelector(this.first, this.second);
+
+  @override
+  String describe() {
+    return '${first.describe()} and ${second.describe()}';
+  }
+
+  @override
+  List<O> select(List<E> elements) {
+    return {
+      ...first.select(elements),
+      ...second.select(elements),
+    }.toList();
   }
 }
